f07388b19ca274947ad7d6ca80a40765f6c92f5e
Hello-Benz <630112418061@bru.ac.th>
2021-12-30 15:32:46 +0700

    LAB7

--- LAB3/src/index.js
+++ LAB3/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from 'react'; /*คำสั่งเริ่มต้นในการใช้ react*/ 
 import ReactDOM from 'react-dom';
-import KAK from './components/Car';
+import KAK from './components/Car'; /*คำสั่งในการ import ไฟล์ cas.js เข้ามาใช้ */
 import Datatable from './components/Table';
 
 ReactDOM.render(
